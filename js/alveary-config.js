eb12d31ca66446826818d1105934032d9c5e3349
kkeel <149272633+kkeel@users.noreply.github.com>
2026-05-11 09:56:18 -0400

    Add ALVEARY_CONFIG for grades, subjects, and tracks

---
+++ js/alveary-config.js
@@ -0,0 +1,73 @@
+window.ALVEARY_CONFIG = {
+  grades: [
+    "All Grades",
+    "G1",
+    "G2",
+    "G3",
+    "G4",
+    "G5",
+    "G6",
+    "G7",
+    "G8",
+    "G9",
+    "G10",
+    "G11",
+    "G12"
+  ],
+
+  subjects: [
+    "All Subjects",
+    "Architecture",
+    "Art",
+    "Bible",
+    "Citizenship",
+    "English",
+    "Geography",
+    "History",
+    "Latin",
+    "Life Skills",
+    "Literature",
+    "Math",
+    "Modern Language",
+    "Music",
+    "Physical Education",
+    "Science",
+    "Alt. Science Options",
+    "Suggested Resources"
+  ],
+
+  tracks: [
+    {
+      value: "",
+      label: "US + Canadian"
+    },
+    {
+      value: "us",
+      label: "US only"
+    },
+    {
+      value: "canadian",
+      label: "Canadian only"
+    }
+  ],
+
+  subjectColors: {
+    "Architecture": "#a0a6be",
+    "Art": "#907061",
+    "Bible": "#964945",
+    "Citizenship": "#62765c",
+    "English": "#9b5b7b",
+    "Geography": "#4d8da2",
+    "History": "#6b6bbf",
+    "Latin": "#5a5373",
+    "Life Skills": "#d1b358",
+    "Literature": "#c07669",
+    "Math": "#6d7eaa",
+    "Modern Language": "#6db4b2",
+    "Music": "#9e6bac",
+    "Physical Education": "#bd855e",
+    "Science": "#96a767",
+    "Alt. Science Options": "#96a767",
+    "Suggested Resources": "#5A5D66"
+  }
+};
